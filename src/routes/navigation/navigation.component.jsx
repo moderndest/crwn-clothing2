945cd613ab171bb569087539fd8a230b75dc749c
moderndest <rattarasan.c@husky.neu.edu>
2022-04-12 16:26:30 -0400

    adding cart context and shoooing icon as well as cart dropdown

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -2,12 +2,19 @@ import { Outlet, Link } from "react-router-dom";
 import { Fragment, useContext } from "react";
 
 import {ReactComponent as CrwnLogo} from '../../assets/crown.svg';
-import './navigation.styles.scss';
-import { UserContext } from "../../contexts/user.context";
 import { signOutUser } from "../../utils/firebase/firebase.utils";
+
+import './navigation.styles.scss';
+
+import { UserContext } from "../../contexts/user.context";
+import { CartContext } from "../../contexts/cart.context";
+
+import CartIcon from "../../components/cart-icon/cart-icon.component";
+import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
 
 const Navigation = () => {
     const {currentUser} = useContext(UserContext);
+    const { isCartOpen } = useContext(CartContext);
 
 
     return (
@@ -28,8 +35,9 @@ const Navigation = () => {
                                     SIGN IN
                                 </Link>
                         )}
-                    
+                    <CartIcon/>
                 </div>
+                {isCartOpen && <CartDropdown/>}
             </div>
             <Outlet />
         </Fragment>
