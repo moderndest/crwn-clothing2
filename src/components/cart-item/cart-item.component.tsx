15f8ff4c78ce2044c06c856226ec11eb859cbf36
moderndest <rattarasan.c@husky.neu.edu>
2022-05-03 15:07:50 -0400

    update all code to typescript

---
+++ src/components/cart-item/cart-item.component.tsx
@@ -0,0 +1,25 @@
+import { CartItemContainer, ItemDetails } from "./cart-item.styles";
+
+import { FC } from "react";
+import { CartItem as TCartItem } from "../../store/cart/cart.types";
+
+type CartItemProps = {
+  cartItem: TCartItem;
+};
+
+const CartItem: FC<CartItemProps> = ({ cartItem }) => {
+  const { name, imageUrl, price, quantity } = cartItem;
+
+  return (
+    <CartItemContainer>
+      <img src={imageUrl} alt={`${name}`} />
+      <ItemDetails>
+        <span className="name">{name}</span>
+        <span className="price">
+          {quantity} * {price}
+        </span>
+      </ItemDetails>
+    </CartItemContainer>
+  );
+};
+export default CartItem;
---
+++ src/routes/category/category.component.tsx
@@ -0,0 +1,50 @@
+import { CategoryContainer, Title } from "./category.styles";
+
+import { Fragment, useEffect, useState } from "react";
+
+import { useSelector } from "react-redux";
+import {
+  selectCategoriesIsLoading,
+  selectCategoriesMap,
+} from "../../store/categories/category.selector";
+
+import ProductCard from "../../components/product-card/product-card.component";
+
+import { useParams } from "react-router-dom";
+
+import Spinner from "../../components/spinner/spinner.component";
+
+type CategoryRouteParams = {
+  category: string;
+};
+
+const Category = () => {
+  const { category } = useParams<
+    keyof CategoryRouteParams
+  >() as CategoryRouteParams;
+  const categoriesMap = useSelector(selectCategoriesMap);
+  const isLoading = useSelector(selectCategoriesIsLoading);
+  const [products, setProducts] = useState(categoriesMap[category]);
+
+  useEffect(() => {
+    setProducts(categoriesMap[category]);
+  }, [category, categoriesMap]);
+
+  return (
+    <Fragment>
+      <Title>{category.toUpperCase()}</Title>
+      {isLoading ? (
+        <Spinner />
+      ) : (
+        <CategoryContainer>
+          {products &&
+            products.map((product) => (
+              <ProductCard key={product.id} product={product} />
+            ))}
+        </CategoryContainer>
+      )}
+    </Fragment>
+  );
+};
+
+export default Category;
